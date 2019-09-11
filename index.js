bef0f233d19828a7fe201dac55e9ddce501c0455
KHilse <khilsedev@gmail.com>
2019-09-11 13:50:25 -0700

    First commit

---
+++ index.js
@@ -0,0 +1,23 @@
+// IMPORTS
+const cors = require('cors');
+const express = require('express');
+
+
+// Instantiate app
+const app = express();
+
+// MIDDLEWARE
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '10mb' }));
+
+// CONTROLLERS
+
+
+// GLOBAL ROUTES
+app.get('*', (req, res) => {
+	res.status(404).send('Didn\'t find anything here');
+})
+
+
+app.listen(process.env.PORT || 8000);
