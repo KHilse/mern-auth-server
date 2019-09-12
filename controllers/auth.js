3ef578ebb518ac38bcb128d0903864b46f6a82f6
KHilse <khilsedev@gmail.com>
2019-09-12 11:12:30 -0700

    Added login route

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,7 +4,29 @@ const db = require('../models');
 const jwt = require('jsonwebtoken');
 
 router.post('/login', (req,res) => {
-	res.send('STUB - auth/login POST');
+	db.User.findOne({ email: req.body.email })
+	.then(user => {
+		// Make sure we have a user and the user has a password
+		if (!user || !user.password) {
+			return res.status(404).send({ message: 'User not found' });
+		}
+
+		// Validate the password
+		if (!user.isAuthenticated(req.body.password)) {
+			// Wrong password
+			return res.status(406).send({ message: 'Not acceptable: Invalid creds' });
+		}
+
+		// Valid user: Assign a token
+		let token = jwt.sign(user.toJSON(), process.env.JWT_SECRET, {
+			expiresIn: 60 * 60 * 12
+		})
+		res.send({ token });
+	})
+	.catch(err => {
+		console.log("ERROR during db lookup of user info");
+		res.status(503).send({ message: 'Something went wrong with the db' });
+	})
 })
 
 router.post('/signup', (req, res) => {
@@ -14,7 +36,7 @@ router.post('/signup', (req, res) => {
 	.then(user => {
 		// if user exists, don't let them set up a dupe account
 		if (user) {
-			return res.status(409).send('Email address already in use on this site');
+			return res.status(409).send({ message: 'Email address already in use on this site' });
 		}
 
 		// User doesn't exist, so create the db entry
@@ -28,12 +50,12 @@ router.post('/signup', (req, res) => {
 		})
 		.catch(err => {
 			console.log("ERROR creating new user record", err);
-			res.status(500).send('Couldn\'t create a new user record');
+			res.status(500).send({ message: 'Couldn\'t create a new user record' });
 		})
 	})
 	.catch(err => {
 		console.log('ERROR: existing user found in signup POST', err);
-		res.status(503).send('Something went wrong with the db');
+		res.status(503).send({ message: 'Something went wrong with the db' });
 	})
 //	res.send('STUB - auth/signup POST');
 })
